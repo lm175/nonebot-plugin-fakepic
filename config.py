1b68909d7bf796e08799d158d5f5c3a09cdcccb9
lm12345 <1756000830@qq.com>
2024-08-13 04:28:22 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,23 @@
+from pydantic import Extra, BaseModel
+from nonebot import get_driver
+
+
+class Config(BaseModel, extra=Extra.ignore):
+
+    """分隔不同用户的符号"""
+    fakepic_user_split: str = "+"
+
+    """分隔相同用户的几条消息"""
+    fakepic_message_split: str = " "
+
+    """是否为用户添加等级图标, 默认为True"""
+    fakepic_add_level_icon: bool = True
+
+    """是否为官方机器人添加bot图标， 默认为True"""
+    fakepic_add_bot_icon: bool = True
+
+    """是否删除QQ表情的CQ码， 默认为True"""
+    fakepic_del_cqface: bool = True
+
+
+config = Config.parse_obj(get_driver().config.dict())
